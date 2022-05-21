7e6aa53944cca638cffa4b81e6740d3459948ef6
iamabigartist <pch4178@gmail.com>
2022-05-21 21:26:35 +0800

    probuilder and the ball cylinder

--- Assets/Labs/TestRoslyn/Editor/TestScriptCompletion.cs
+++ Assets/Labs/TestRoslyn/Editor/TestScriptCompletion.cs
@@ -3,7 +3,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.Scripting.Hosting;
 using UnityEditor;
 using UnityEngine;
 namespace Labs.TestRoslyn.Editor
@@ -23,8 +22,8 @@ namespace Labs.TestRoslyn.Editor
 			var host = MefHostServices.Create(MefHostServices.DefaultAssemblies);
 			var workspace = new AdhocWorkspace(host);
 			var scriptCode = "using System; Guid.N";
-			var scriptCode0 = "using System; (int font_size, float light) Monoid=(1,1f);";
-			var scriptCode1 = "Monoid.";
+			var scriptCode0 = "using System; int asdkfjisj=1;";
+			var scriptCode1 = "asdk";
 			var compilationOptions = new CSharpCompilationOptions(
 				OutputKind.DynamicallyLinkedLibrary);
 			var metadataReferences = new[]
@@ -41,22 +40,21 @@ namespace Labs.TestRoslyn.Editor
 			var scriptProject = workspace.AddProject(scriptProjectInfo);
 			var scriptDocument = scriptProject.AddDocument("script_0", scriptCode0);
 
-			scriptProject = workspace.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "project_1", "project_1", LanguageNames.CSharp, isSubmission: true).
+			var scriptProject1 = workspace.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "project_1", "project_1", LanguageNames.CSharp, isSubmission: true).
 				WithMetadataReferences(metadataReferences).
 				WithCompilationOptions(compilationOptions).
 				WithParseOptions(csharpParseOptions).WithProjectReferences(new[]
 				{
 					new ProjectReference(scriptProject.Id)
 				}));
-			
-			new InteractiveScriptGlobals();
 
-			
-			scriptDocument = scriptProject.AddDocument("script_1", scriptCode1);
+
+
+			var scriptDocument1 = scriptProject1.AddDocument("script_1", scriptCode1);
 
 			var caret_position = scriptCode1.Length - 1;
-			var completionService = CompletionService.GetService(scriptDocument);
-			var results = await completionService.GetCompletionsAsync(scriptDocument, caret_position);
+			var completionService = CompletionService.GetService(scriptDocument1);
+			var results = await completionService.GetCompletionsAsync(scriptDocument1, caret_position);
 
 			results.Items.ToList().ForEach(item =>
 			{
---
+++ Assets/Labs/TestSourceGen/Editor/TestMySourceGen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace Labs.TestSourceGen.Editor
+{
+	public class TestMySourceGen : EditorWindow
+	{
+		[MenuItem("Labs/Labs.TestSourceGen.Editor/TestMySourceGen")]
+		static void ShowWindow()
+		{
+			var window = GetWindow<TestMySourceGen>();
+			window.titleContent = new("TestMySourceGen");
+			window.Show();
+		}
+
+		void OnEnable()
+		{
+			HelloWorldGenerated.HelloWorld.SayHello();
+		}
+
+		void OnGUI()
+		{
+
+		}
+	}
+}
